5d035a3f48569e5650bf3b19e9e53e564bafaf0b
ChrisBotos <130077292+ChrisBotos@users.noreply.github.com>
2024-03-27 16:56:09 +0200

    Uploading the first version to git

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ This way you can display different graphs on top of each other.
 
 # How to install
 This package will be added to pip so a simple "pip install dynamic_pygame_graphs" would suffice.
-Otherwise clone this repository.
+Otherwise, clone this repository.
 
 # How to use
 Take a look at the example_usage.py too.
@@ -26,24 +26,24 @@ pygame_graphs_instance = dynamic_pygame_graphs_class(x, y, screen)
 Afterwards you can use this instance to draw a graph of your choosing on screen.
 
 Example:
-pygame_graphs_instance.pygame_graphs_instance.dynamic_histogram(x_values=np.array([1,0,2,3,0,1]),
-                                                                bin_size=1,
-                                                                bar_color=(255, 0, 0, 123),
-                                                                graph_x=500,
-                                                                graph_y=500,
-                                                                graph_x_axis_name='',
-                                                                graph_y_axis_name='',
-                                                                x_indicator_points=5,
-                                                                y_indicator_points=5,
-                                                                y_amplifier=1,
-                                                                graph_indicators_font=None,
-                                                                graph_indicators_text_color=(0, 0, 0),
-                                                                graph_indicators_text_space_from_x_axis=10,
-                                                                graph_indicators_text_space_from_y_axis=20,
-                                                                move_zero_along_x_axis=0,
-                                                                move_zero_along_x_axis_indicator_text_color=(0, 0, 0),
-                                                                bin_array_is_given_as_x_values=True,
-                                                                have_extra_bin=True):
+pygame_graphs_instance.dynamic_histogram(x_values=np.array([1, 0, 2, 3, 0, 1]),
+                                         bin_size=1,
+                                         bar_color=(255, 0, 0, 123),
+                                         graph_x=500,
+                                         graph_y=500,
+                                         graph_x_axis_name='',
+                                         graph_y_axis_name='',
+                                         x_indicator_points=5,
+                                         y_indicator_points=5,
+                                         y_amplifier=1,
+                                         graph_indicators_font=None,
+                                         graph_indicators_text_color=(0, 0, 0),
+                                         graph_indicators_text_space_from_x_axis=10,
+                                         graph_indicators_text_space_from_y_axis=20,
+                                         move_zero_along_x_axis=0,
+                                         move_zero_along_x_axis_indicator_text_color=(0, 0, 0),
+                                         bin_array_is_given_as_x_values=True,
+                                         have_extra_bin=True)
 
 Of course you do not need to fill all those variables, they all have pregiven values.
 As for what exactly all of them mean refer to the comments in the actual code.
---
+++ example_usage.py
@@ -0,0 +1,85 @@
+import pygame
+import numpy as np
+from dynamic_graphs_with_pygame import dynamic_pygame_graphs_class
+
+# Initialize Pygame
+pygame.init()
+
+# Set up the display
+WIDTH, HEIGHT = 1200, 700
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Randomized NumPy Array")
+
+# Make the instance
+x = 30
+y = 30
+pygame_graphs_instance = dynamic_pygame_graphs_class(x, y, screen)
+
+# Main loop
+running = True
+while running:
+
+    # Handle events.
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Clear the screen with white color.
+    screen.fill((255, 255, 255))
+
+    # Scrolling with the arrow keys.
+    pygame_graphs_instance.scroll(scrolling_speed=10)
+
+    # Generate a new randomized NumPy array.
+    randomized_array = np.random.uniform(0, 230, 300)
+
+    # Drawing the graph.
+    pygame_graphs_instance.dynamic_histogram(x_values=randomized_array,
+                                             bin_size=1,
+                                             bar_color=(255, 0, 0),
+                                             graph_x=500,
+                                             graph_y=500,
+                                             graph_x_axis_name='',
+                                             graph_y_axis_name='',
+                                             x_indicator_points=5,
+                                             y_indicator_points=5,
+                                             y_amplifier=1,
+                                             graph_indicators_font=None,
+                                             graph_indicators_text_color=(0, 0, 0),
+                                             graph_indicators_text_space_from_x_axis=10,
+                                             graph_indicators_text_space_from_y_axis=20,
+                                             move_zero_along_x_axis=0,
+                                             move_zero_along_x_axis_indicator_text_color=(0, 0, 0),
+                                             bin_array_is_given_as_x_values=True,
+                                             have_extra_bin=True)
+
+    # Generate a new randomized NumPy array.
+    randomized_array = np.zeros(500)
+    randomized_array[200:] = np.random.uniform(0, 460, 300)
+
+    # Drawing the second graph to show that you can make multiple and let them overlap.
+    # Also shows the usefulness of alpha_coloring (look at the fourth value in the color).
+    pygame_graphs_instance.dynamic_histogram(x_values=randomized_array,
+                                             bin_size=10,
+                                             bar_color=(0, 0, 255, 123),
+                                             graph_x=500,
+                                             graph_y=500,
+                                             graph_x_axis_name='',
+                                             graph_y_axis_name='',
+                                             x_indicator_points=5,
+                                             y_indicator_points=5,
+                                             y_amplifier=1,
+                                             graph_indicators_font=None,
+                                             graph_indicators_text_color=(0, 0, 0),
+                                             graph_indicators_text_space_from_x_axis=10,
+                                             graph_indicators_text_space_from_y_axis=20,
+                                             move_zero_along_x_axis=0,
+                                             move_zero_along_x_axis_indicator_text_color=(0, 0, 0),
+                                             bin_array_is_given_as_x_values=True,
+                                             have_extra_bin=True)
+
+    # Update the display
+    pygame.display.flip()
+
+# Quit Pygame
+pygame.quit()
