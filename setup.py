b7add5fd582c5e2cb7d4875baaa5c84ee48f26a4
ChrisBotos <130077292+ChrisBotos@users.noreply.github.com>
2024-03-30 17:55:43 +0200

    Uploading this version to git.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="dynamic_graphs_with_pygame",
-    version="1.0.7",
+    version="1.0.8",
     description="A class for drawing dynamic graphs using Pygame.",
     long_description=long_description,
     long_description_content_type="text/markdown",
